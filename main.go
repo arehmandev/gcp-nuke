224889615c4a6fda165c9004ef3f540d9e7824b6
abdrehma <abdrehma@publicisgroupe.net>
2019-12-17 18:05:27 +0000

    Initial commit

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/arehmandev/gcp-nuke/config"
@@ -20,18 +22,36 @@ func removeProject(config config.Config) {
 
 	for _, resource := range resourceMap {
 		if len(resource.List()) > 0 {
-			recursiveDeletion(resourceMap, resource)
+			err := recursiveDeletion(resourceMap, resource, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 }
 
-func recursiveDeletion(resourceMap map[string]gcp.Resource, resource gcp.Resource) {
+func recursiveDeletion(resourceMap map[string]gcp.Resource, resource gcp.Resource, recursionList []gcp.Resource) error {
+
+	if sliceContainsResource(recursionList, resource) {
+		return fmt.Errorf("resource %v has broken recursion as it is already in the recursion chain: %v", resource.Name(), recursionList)
+	}
+	recursionList = append(recursionList, resource)
 
 	if len(resource.Dependencies()) > 0 {
 		for _, dependency := range resource.Dependencies() {
-			recursiveDeletion(resourceMap, resourceMap[dependency])
+			recursiveDeletion(resourceMap, resourceMap[dependency], recursionList)
 		}
 	}
 	resource.Remove()
 
+	return nil
+}
+
+func sliceContainsResource(inputList []gcp.Resource, input gcp.Resource) bool {
+	for _, value := range inputList {
+		if value == input {
+			return true
+		}
+	}
+	return false
 }
